3674010b2333038ccb55e42942dd84c0616f1335
Mandella-thi <73672494+Mandella-thi@users.noreply.github.com>
2023-02-17 14:40:47 -0300

    "Flatmate web preenchimento de campos e resultado parcial"

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask.views import MethodView
-from wtforms import Form, StringField
-from flask import Flask, render_template
+from wtforms import Form, StringField, SubmitField
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -13,11 +13,22 @@ class BillFormPage(MethodView):
         return render_template('bill_form_page.html', billform= bill_form)
 
 class ResultsPage(MethodView):
-    pass
+    def post(self):
+        billform = BillForm(request.form)
+        amount=billform.amount.data
+        return amount
 class BillForm(Form):
     amount = StringField("Bill Amount: ")
     period = StringField("Bill Period: ")
+
+    name1= StringField("Name: ")
+    days_in_house1 =StringField("Days in the house: ")
+
+    name2 = StringField("Name: ")
+    days_in_house2 = StringField("Days in the house: ")
+    button = SubmitField("Calculate")
+
 app.add_url_rule('/', view_func= HomePage.as_view('home_page'))
 app.add_url_rule('/bill', view_func= BillFormPage.as_view('bill_form_page'))
-
+app.add_url_rule('/results', view_func= ResultsPage.as_view('results_page'))
 app.run()
